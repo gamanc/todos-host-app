a3f8ed138225b51f236fed87e00cf735b5d32839
Emerson N <gammanolasco7@gmail.com>
2024-06-05 00:02:40 -0600

    feat: update vite.config to expose components as mf

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "todo-components",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./List": "./src/components/List",
+        "./Input": "./src/components/Input",
+        "./TodoType": "./src/interfaces/Todo.ts",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
