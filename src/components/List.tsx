f158dbbc4aa40daea2cf15fbeb0d64413fb5c7ce
Emerson N <gammanolasco7@gmail.com>
2024-06-04 22:55:27 -0600

    feat: update list to show each item strikethrough accordingly

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,18 +2,37 @@ import { Todo } from "../interfaces/Todo";
 
 interface Props {
   items: Todo[];
+  onTodoChange: (id: string, completed: boolean) => void;
 }
 
-const List = ({ items }: Props) => {
+const List = ({ items, onTodoChange }: Props) => {
+  const handleCheckboxChange = (id: string, completed: boolean) => {
+    onTodoChange(id, completed);
+  };
+
   return (
-    <ul>
-      {items.map(({ id, name, completed }) => (
-        <li>
-          <input type="checkbox" id={id} checked={completed} />
-          <label htmlFor={id}>{name}</label>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {items.map(({ id, name, completed }) => (
+          <li key={id}>
+            <input
+              type="checkbox"
+              id={id}
+              checked={completed}
+              onChange={(e) => handleCheckboxChange(id, e.target.checked)}
+            />
+            <label
+              htmlFor={id}
+              style={{
+                textDecoration: completed ? "line-through" : "none",
+              }}
+            >
+              {name}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
