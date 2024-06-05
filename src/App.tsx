001f632ef3596fe83f615aa9581ac7847cc2ca35
Emerson N <gammanolasco7@gmail.com>
2024-06-04 22:55:58 -0600

    feat: implemetatin of both list and input components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import { useState } from "react";
-import List from "./components/List";
-import { Todo } from "./interfaces/Todo";
 import { v4 } from "uuid";
+
+import List from "./components/List";
+import Input from "./components/Input";
+import { Todo } from "./interfaces/Todo";
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([
-    { id: v4(), name: "Buy milk", completed: false },
+    { id: v4(), name: "Buy milk", completed: true },
   ]);
+
+  const [todoName, setTodoName] = useState<string>("");
+
+  const addTodo = () => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: v4(), name: todoName, completed: false },
+    ]);
+    setTodoName("");
+  };
+
+  const checkTodo = (id: string, completed: boolean) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => (todo.id === id ? { ...todo, completed } : todo))
+    );
+  };
 
   return (
     <>
-      <List items={todos} />
+      <Input value={todoName} onChange={setTodoName} onSubmit={addTodo} />
+      <List items={todos} onTodoChange={checkTodo} />
     </>
   );
 }
